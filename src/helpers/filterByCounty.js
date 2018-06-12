695dd0de282c9f686b7a2312e8c070be4b24bdf0
Corey Lewis <lewisc503@gmail.com>
2018-06-12 11:07:24 -0700

    Updates

--- src/helpers/filterByCounty.js
+++
@@ -1,6 +0,0 @@
-export const filterByCounty = data => {
-  var counties = data.filter(obj =>
-    /Multnomah|Washington|Marion|Yamhill|Clackamas/.test(obj.county)
-  );
-  return counties;
-};
--- src/helpers/filterResults.js
+++
@@ -1,4 +0,0 @@
-export const filterResults = data => {
-  var dataWithUnits = data.filter(arr => arr[19] !== "No Units");
-  return dataWithUnits.filter(arr => arr[13] === "Oregon");
-};
